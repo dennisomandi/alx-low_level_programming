37201400c3e46844860e40b07c77942e9ae20842
dennisomandi <ormandydennis@gmail.com>
2022-03-15 11:59:29 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include "unistd.h"
 /**
- *  * main- prints _putchar followed by a new line
+ *  * main - Prints _putchar
  *
  *  *
  *
- *  * Return: 0
+ *  * Return: Always 0 (Success)
  *
  **/
 int main(void)
-{_putchar('_');
+{
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * print_alphabet- prints the alphabet
+ *
+ *  *
+ *
+ *  * Return: 0
+ *
+ **/
+void print_alphabet(void)
+{
+char c = 'a';
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return;
+}
