660253ebde11c8acb02eb3e04a0626d75811920e
dennisomandi <ormandydennis@gmail.com>
2022-03-30 14:26:40 -0700

    a function that prints a string in reverse.

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_puts_recursion("Puts with recurson");
+return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+In this directory we deal on how to make Recursions enjoy.
