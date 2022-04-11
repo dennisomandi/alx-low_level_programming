75db6b779424b27d0d8f0eee66d4101643d71b6a
dennisomandi <ormandydennis@gmail.com>
2022-04-11 08:33:29 -0700

    submit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- *  writes the name file
- **/
-int main (void)
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0;
+ */
+int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n", __FILE__);
 return (0);
 }
