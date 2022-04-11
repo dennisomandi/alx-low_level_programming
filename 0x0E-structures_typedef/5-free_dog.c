23d45f0c17e33366ba35ce495f4710c8079ee8e0
dennisomandi <ormandydennis@gmail.com>
2022-04-11 12:53:27 -0700

    submit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory allocated for structure variable dog_t
+ * @d: pointer to struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+if (d)	/* if truthy */
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
