42df2ec510762c9ca5abc131bba20b699eb0808f
dennisomandi <ormandydennis@gmail.com>
2022-06-09 13:52:57 -0700

    submit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+/*
+ * File 0-print_dlistint.c
+ */
+
+#include "list.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
