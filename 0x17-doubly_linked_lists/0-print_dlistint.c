c3d3f978e61d4f4915c640354223e54cb4e40d3a
dennisomandi <ormandydennis@gmail.com>
2022-06-09 13:59:06 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,17 +3,14 @@
  */
 
 #include "list.h"
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-
-	return (nodes);
+size_t nodes = 0;
+while (h)
+{
+nodes++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (nodes);
 }
